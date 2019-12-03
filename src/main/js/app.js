6628e36847503c2a4da3abe3552032f40d97d44b
dalvarez <dalvarez@ec.ibm.com>
2019-12-02 23:15:19 -0500

    added dotenv module to use configurations from .env file

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -1,6 +1,6 @@
 'use strict';
-const Logger = require('./controller/controller.js').Logger;
+require('dotenv').config();
 const Controller = require('./controller/controller.js').Controller;
 
-var logger = new Logger();
+
 var controller = new Controller();
